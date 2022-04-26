38303186356bd615e51c79138d49777ac1570b7a
Neo <fullstackneo@gmail.com>
2022-04-25 23:13:01 -0600

    modify relations of models

--- models/index.js
+++ models/index.js
@@ -4,16 +4,11 @@ const Vote = require('./Vote');
 const Comment = require('./Comments');
 
 User.hasMany(Post, {
-  foreignKey: 'user_id'
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL',
 });
 
 Post.belongsTo(User, {
-  foreignKey: 'user_id',
-});
-
-User.belongsToMany(Post, {
-  through: Vote,
-  as: 'voted_posts',
   foreignKey: 'user_id',
 });
 
@@ -23,28 +18,29 @@ Post.belongsToMany(User, {
   foreignKey: 'post_id',
 });
 
-Vote.belongsTo(User, {
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
   foreignKey: 'user_id',
 });
 
-Vote.belongsTo(Post, {
-  foreignKey: 'post_id',
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL',
 });
 
 Comment.belongsTo(User, {
   foreignKey: 'user_id',
 });
 
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+});
+
 Comment.belongsTo(Post, {
   foreignKey: 'post_id',
 });
 
-User.hasMany(Comment, {
-  foreignKey: 'user_id',
-});
 
-Post.hasMany(Comment, {
-  foreignKey: 'post_id',
-});
 
 module.exports = { User, Post, Vote, Comment };
